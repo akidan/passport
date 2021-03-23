ca5be71ee103b362f7207dbbb9167ed1f7d19ba9
akidan <zhiyue@gmail.com>
2021-03-23 21:14:33 +0900

    v1.1 add reservation history

--- passport.py
+++ passport.py
@@ -23,7 +23,9 @@ def main():
 
     fp1 = htdocs+'.html'
     fp2 = htdocs+'_log.html'
-    fp3 = htdocs+'_history.html'
+    fp3 = htdocs+'_log_tmp.html'
+    fp4 = htdocs+'_history.html'
+    fp5 = htdocs+'_history_origin.html'
     try:
         response = requests.post(
             'https://ppt.mfa.gov.cn/appo/service/reservation/data/getReservationDateBean.json',
@@ -36,9 +38,13 @@ def main():
     timelist = list()
     result = False
     lasttime = ""
-    history_prev = ""
-    history_post = ""
-    history  = ""
+    #fp3 format
+    #Line1: [2021-03-22 06:22:09] 
+    #Line2: 放出1个空位 | 4月16日 10:00-11:00 1个
+    now_line1 = ""
+    now_line2 = ""
+    past_line1 = ""
+    past_line2 = ""
     all_count = 0
     for s in schedule:
         day_per_count = 0
@@ -46,7 +52,7 @@ def main():
             hour_per_count = t['peopleNumber']-t['userNumber']
             if hour_per_count > 0:
                 day_per_count += hour_per_count
-                history_post += ' | '+s['date'][6:].replace('-0', '月').replace('-', '月')+'日 '+t['startTime']+'-'+t['endTime']+' '+str(t['peopleNumber']-t['userNumber'])+'个'
+                now_line2 += ' | '+s['date'][6:].replace('-0', '月').replace('-', '月')+'日 '+t['startTime']+'-'+t['endTime']+' '+str(t['peopleNumber']-t['userNumber'])+'个'
 
         if day_per_count == 0:
             timelist.append(s['date'] +' 可预约数: '+str(day_per_count)+'\n')
@@ -54,40 +60,55 @@ def main():
             all_count += day_per_count
             timelist.append(s['date']+' <font color="red">可预约数: '+str(day_per_count)+'</font>\n')
             if result == False:
-                lasttime += '上次放号时间: '+time.strftime("%Y-%m-%d %H:%M", time.localtime())+'\n'
+                lasttime += '<a href=log>上次放号时间</a>: '+time.strftime("%Y-%m-%d %H:%M", time.localtime())+'\n'
             result = True
             #lasttime = lasttime+'于 '+s['date'] +' 时间段放出 '+str(day_per_count)+' 个空位'+'\n'
 
-    if all_count > 0:
-        history_prev = '['+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+'] 放出'
-        history = history_prev+str(all_count)+'个空位'+history_post
-        f3 = open(fp3, 'r+', encoding='UTF-8')
-        oldhistory = f3.read()        
-        f3.seek(0, 0)
-        f3.write(history+'\n'+oldhistory)
-        f3.close()
+    now_line1 = '['+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+']'
+    now_line2 = '放出'+str(all_count)+'个空位'+now_line2
+        
+    with open(fp3, 'r', encoding='UTF-8') as f3:
+        past_line1 = f3.readline().strip('\n')
+        past_line2 = f3.readline().strip('\n')
 
+    #past  now
+    # 0     0    Keep
+    # N     N    Keep
+    # 0     N    Write f3
+    # N1    N2   Write f3,f4,f5
+    # N     0    Write f3,f4,f5
+    if past_line2 != now_line2:
+        with open(fp3, 'w', encoding='UTF-8') as f3:
+            f3.write(now_line1+'\n'+now_line2)
+        if past_line2 != '放出0个空位':
+            append_to_head(fp4, past_line1, past_line2, now_line1)
+            append_to_head(fp5, past_line1, past_line2, now_line1)
+            
     if lasttime != "":
-        f2 = open(fp2, 'w', encoding='UTF-8')
-        f2.write(lasttime)
-        f2.close()
+        with open(fp2, 'w', encoding='UTF-8') as f2:
+            f2.write(lasttime)
 
     timelist.sort()
-    f1 = open(fp1, 'w', encoding='UTF-8')
-    f1.write('<h2>更新护照预约空位查询</h2>')
-    if result == False:
-        f1.write('当前状态: 无空位\n')
-        f1.write('网页更新时间: '+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+'\n')
-        f2 = open(fp2, 'r', encoding='UTF-8')
-        f1.write(f2.read()+'\n')
-    else:
-        f1.write('<font color="red">当前状态: 有空位</font>\n')
-        f1.write('网页更新时间: '+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+'\n')
-        f1.write('<a href=https://ppt.mfa.gov.cn/appo/index.html>点此快速预约</a>\n\n')
-    for l in timelist:
-        f1.write(l)
-    f1.close()
+    with open(fp1, 'w', encoding='UTF-8') as f1:
+        f1.write('<h2>更新护照预约空位查询</h2>')
+        if result == False:
+            f1.write('当前状态: 无空位\n')
+            f1.write('网页更新时间: '+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+'\n')
+            with open(fp2, 'r', encoding='UTF-8') as f2:
+                f1.write(f2.read()+'\n')
+        else:
+            f1.write('<font color="red">当前状态: 有空位</font>\n')
+            f1.write('网页更新时间: '+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+'\n')
+            f1.write('<a href=https://ppt.mfa.gov.cn/appo/index.html>点此快速预约</a>\n\n')
+        for l in timelist:
+            f1.write(l)
 
+def append_to_head(fp, t1, t2, now):
+    with open(fp, 'r+', encoding='UTF-8') as f:
+        text = f.read()
+        f.seek(0, 0)
+        f.write(t1[:-1]+'-'+now[-9:]+' '+t2+'\n'+text)
+            
 if __name__ == "__main__":
     while (True):
         main()
